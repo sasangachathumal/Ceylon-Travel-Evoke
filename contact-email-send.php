e53f679d38964798753574e886b4c5591c57a291
SMSC <chathumal.sasanga02@gmail.com>
2017-11-18 00:50:45 +0530

    added another email to send emails.

--- contact-email-send.php
+++ contact-email-send.php
@@ -21,15 +21,23 @@ else {
     $phone = $_POST['Phone'];
     $messageBody = $_POST['message'];
 
-    $myMessage = '<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css"></head><body><div class="container-fluid"><div class="jumbotron" style="margin-bottom: 0"><h1>Ceylon Travel Evoke</h1><h3>Customer Contact Service</h3><h4>Please contact this customer as soon as possible.</h4></div><div class="jumbotron" style="background-color: #ddd !important"><table><tr><td class="col-md-6"><h3>Customer Name</h3></td><td>'.$name.'</td></tr><tr><td class="col-md-6"><h3>Customer Email</h3></td><td>'.$email.'</td></tr><tr><td class="col-md-6"><h3>Customer Phone</h3></td><td>'.$phone.'</td></tr><tr><td class="col-md-6"><h3>Customer Message</h3></td><td>'.$messageBody.'</td></tr></table></div></div></body></html>';
+    $myMessage = '<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css"></head><body><div class="container-fluid"><div class="jumbotron" style="margin-bottom: 0"><h1>Ceylon Travel Evoke</h1><h3>Customer Contact Service</h3><h4>Please contact this customer as soon as possible.</h4></div><div class="jumbotron"><table><tr><td class="col-md-6"><h3>Customer Name</h3></td><td>'.$name.'</td></tr><tr><td class="col-md-6"><h3>Customer Email</h3></td><td>'.$email.'</td></tr><tr><td class="col-md-6"><h3>Customer Phone</h3></td><td>'.$phone.'</td></tr><tr><td class="col-md-6"><h3>Customer Message</h3></td><td>'.$messageBody.'</td></tr></table></div></div></body></html>';
 
     try {
         $message = new Message();
         $message->setSender('contact@ceylontravelevoke.appspotmail.com');
-        $message->addTo('chathumal.sasanga02@gmail.com');
+        $message->addTo('rangikamadu.dl27@gmail.com');
         $message->setSubject('Ceylon Travel Evoke contact us');
         $message->setHtmlBody($myMessage);
         $message->send();
+
+        $message = new Message();
+        $message->setSender('contact@ceylontravelevoke.appspotmail.com');
+        $message->addTo('hasitharanjana@gmail.com');
+        $message->setSubject('Ceylon Travel Evoke contact us');
+        $message->setHtmlBody($myMessage);
+        $message->send();
+
         echo 'success';
     } catch (InvalidArgumentException $e) {
         echo 'error';
